1298db6eeafccdd95dd091b443c3f050d27159f4
clarkmyfancy <clark.jack0@gmail.com>
2019-12-17 17:37:39 -0600

    created basic sudoku puzzle ui structure

--- script.py
+++ script.py
@@ -1,25 +1,19 @@
 from Puzzle import Puzzle
+from Board import Board
 import pygame
-
+pygame.font.init()
 
 def main():
     WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-
-    DARK_GREEN = (31, 159, 91)
-    BLUE = (0, 0, 255)
     BLACK = (0, 0, 0)
-    # puzzle = Puzzle()
-    # puzzle.print_board()
-   
-    # puzzle.solve()
-    # print()
-    # puzzle.print_board()
-    pygame.init()
-    size = (700, 500)
+    
+    CUSTOM_FONT = pygame.font.SysFont('Times New Roman', 30)
+    
+
+
+    size = (500, 500)
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("My first game")
+    pygame.display.set_caption("SudoKu")
 
     clock = pygame.time.Clock()
     playing = True
@@ -35,10 +29,32 @@ def main():
         # Drawing code should go Here
 
         screen.fill(WHITE)
-        pygame.draw.rect(screen, DARK_GREEN, [0, 0, 150, 500], 0)
-        # pygame.draw.rect(screen, DARK_GREEN, [0, 0, 150, 500], 0)
-        pygame.draw.rect(screen, DARK_GREEN, [160, 0, 150, 500], 0)
-     
+        width = 500
+        cell_size = (width / 9) - 1
+        for i in range(10):
+            # this is for the bold lines
+            if i % 3 == 0:
+                pygame.draw.rect(screen, BLACK, [i*cell_size, 0, 10, 500], 0)
+                pygame.draw.rect(screen, BLACK, [0, i*cell_size, 500, 10], 0)
+
+            else:
+                pygame.draw.rect(screen, BLACK, [i*cell_size, 0, 2, 500], 0)
+                pygame.draw.rect(screen, BLACK, [0, i*cell_size, 500, 2], 0)
+
+        demo_number_a = CUSTOM_FONT.render("1", False, (0, 0, 0))
+        demo_number_b = CUSTOM_FONT.render("3", False, (0, 0, 0))
+        demo_number_c = CUSTOM_FONT.render("2", False, (0, 0, 0))
+        demo_number_d = CUSTOM_FONT.render("8", False, (0, 0, 0))
+        demo_number_e = CUSTOM_FONT.render("5", False, (0, 0, 0))
+        demo_number_f = CUSTOM_FONT.render("3", False, (0, 0, 0))
+
+        number_placement = (cell_size / 2) - 2
+        screen.blit(demo_number_a, (number_placement*2, number_placement*2))
+        screen.blit(demo_number_b, (number_placement*2*2, number_placement*2*2))
+        screen.blit(demo_number_c, (number_placement*2*2*2, number_placement*2*2*2))
+        screen.blit(demo_number_d, (number_placement, number_placement))
+        screen.blit(demo_number_e, (number_placement, number_placement))
+        screen.blit(demo_number_f, (number_placement, number_placement))
        
 
         # update the screen
