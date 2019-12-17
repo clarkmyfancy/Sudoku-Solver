55537acf389f2322b81565757880c97c097bf861
clarkmyfancy <clark.jack0@gmail.com>
2019-12-17 14:41:25 -0600

    finds solution to puzzle using backtracking

--- Puzzle.py
+++ Puzzle.py
@@ -15,24 +15,57 @@ class Puzzle:
                 [0, 0, 5, 0, 0, 8, 0, 0, 2]
             ]
 
-        
-
-    # def generate_alternative_numbers_for(self, grid):
-    #     pass
-
-    def choose_number_to_try_here(self):
-        # need to check remaining possible choices for this number
-        # if reminin g choices get exhausted:
-        pass
-
     def print_board(self):
         for x in range(9):
             print(self.grid[x])
-            print()
 
-    def attempt_to_solve(self):
-        pass
+    def solve(self):
+        found_empty_box_location = self.find_empty_box()
+        if not found_empty_box_location:
+            return True
+        else: 
+            row, column = found_empty_box_location
 
-    def generate_candidate_answers():
-        self.candidate_answers = generate_candidate_answers()
-        pass
+        for candidate in range(1, 10):
+            if self.valid_candidate_for_position(candidate, (row, column)):
+                self.grid[row][column] = candidate
+
+                if self.solve():
+                    return True 
+            
+                self.grid[row][column] = 0
+        return False
+
+    def find_empty_box(self):
+        for row in range(9):
+            for column in range(9):
+                if self.grid[row][column] == 0:
+                    return (row, column)
+        return None
+    
+    def valid_candidate_for_position(self, candidate, position):
+        if self.passed_horizontal_rule(candidate, position) and self.passed_vertical_rule(candidate, position) and self.passed_neighbor_rule(candidate, position):
+            return True
+        return False
+    
+    def passed_horizontal_rule(self, candidate, position):
+        for i in range(9):
+            if self.grid[position[0]][i] == candidate and position[1] != i:
+                return False
+        return True
+        
+    def passed_vertical_rule(self, candidate, position):
+        for i in range(9):
+            if self.grid[i][position[1]] == candidate and position[0] != i:
+                return False
+        return True
+        
+    def passed_neighbor_rule(self, candidate, position):
+        box_x = position[1] // 3
+        box_y = position[0] // 3
+
+        for i in range(box_y*3, box_y*3 + 3):
+            for j in range(box_x*3, box_x*3 + 3):
+                if self.grid[i][j] == candidate and (i, j) != position:
+                    return False
+        return True
