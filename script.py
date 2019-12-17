6e736abc2d5a478d71d93a3b5073da9896d026b0
clarkmyfancy <clark.jack0@gmail.com>
2019-12-17 14:40:45 -0600

    prints unsolved and solved boards to screen

--- script.py
+++ script.py
@@ -1,34 +1,13 @@
 from Puzzle import Puzzle
 
-
-
 def main():
-
-    
-
-    # rules = ?
     puzzle = Puzzle()
-
     puzzle.print_board()
    
+    puzzle.solve()
+    print()
+    puzzle.print_board()
     
-    # while there is more board to solve
-        # pick new number to try in current location
-        # if it violates rule, 
-            # pick new number from the set of remaingin possible choices 
-        # if run out of elements in the set of possible choices
-            # backtrack to last candidate_sqare and repick their answer
-    while puzzle.is_not_solved:
-        puzzle.attempt_to_solve()
-        for y in range(len(candidate_puzzle)):
-            for x in range(len(candidate_puzzle[y])):
-                puzzle.choose_number_to_try_here()
-             
-                   
-                        # __
-                    number = candidate_puzzle[x][y]
-        board_is_not_solved = False
-
 
 if __name__ == "__main__":
     main()
