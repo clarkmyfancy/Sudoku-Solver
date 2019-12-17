284a6070c0bb806a9c4ef976662db603720131a2
clarkmyfancy <clark.jack0@gmail.com>
2019-12-17 17:37:21 -0600

    basics board for the UI

---
+++ Board.py
@@ -0,0 +1,7 @@
+class Board:
+
+    def __init__(self, rows, cols, height, width, solved = False):
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
