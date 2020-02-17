ac1678defa4a39811be322363eec37bd6c03ff87
clarkmyfancy <clark.jack0@gmail.com>
2020-02-16 23:32:01 -0600

    game loop working correctly

--- script.py
+++ script.py
@@ -1,34 +1,60 @@
-from Puzzle import Puzzle
+import numpy as np
+import pygame
 
+from Gui import Gui
+from Solver import Solver
 
+# grid = np.array([
+#         [0,6,9,0,0,0,0,0,0],
+#         [0,0,7,6,0,0,0,5,0],
+#         [0,2,0,0,0,1,0,0,7],
+#         [2,0,1,0,0,0,4,0,6],
+#         [0,0,0,0,0,2,0,0,9],
+#         [0,0,0,0,8,0,0,0,2],
+#         [0,0,0,3,0,0,1,0,0],
+#         [8,1,0,5,4,0,0,0,0],
+#         [0,0,0,7,0,0,0,9,0]
+
+#     ])
+
+grid = np.array([
+    [0,0,0,9,0,0,0,0,0],
+    [8,3,0,0,7,0,6,0,0],
+    [0,4,0,0,0,0,8,0,7],
+    [0,0,0,7,5,4,0,0,8],
+    [1,5,7,0,0,0,0,9,6],
+    [2,8,0,1,0,0,0,0,0],
+    [0,6,0,0,9,0,1,0,5],
+    [4,1,0,0,8,0,0,0,0],
+    [9,0,8,0,0,0,0,7,0]
+])
+WIDTH = 600
+HEIGHT = 600
 
 def main():
+    surface = (WIDTH, HEIGHT)
+    window = pygame.display.set_mode(surface)
+    pygame.display.set_caption("Sudoku Solver")
 
-    
+    GUI = Gui(WIDTH, HEIGHT)
+    solver = Solver(grid, window, GUI)
 
-    # rules = ?
-    puzzle = Puzzle()
-
-    puzzle.print_board()
-   
-    
-    # while there is more board to solve
-        # pick new number to try in current location
-        # if it violates rule, 
-            # pick new number from the set of remaingin possible choices 
-        # if run out of elements in the set of possible choices
-            # backtrack to last candidate_sqare and repick their answer
-    while puzzle.is_not_solved:
-        puzzle.attempt_to_solve()
-        for y in range(len(candidate_puzzle)):
-            for x in range(len(candidate_puzzle[y])):
-                puzzle.choose_number_to_try_here()
-             
-                   
-                        # __
-                    number = candidate_puzzle[x][y]
-        board_is_not_solved = False
-
+    running = True
+    waiting = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+        if not waiting:
+            GUI.renderPuzzle(window, grid, False)
+            solvedGrid = solver.solve(window)
+            if solver.wasSolved:
+                GUI.renderPuzzle(window, solvedGrid, True)
+                pygame.display.update()
+                waiting = True
 
 if __name__ == "__main__":
     main()
+    pygame.quit()
+
+
