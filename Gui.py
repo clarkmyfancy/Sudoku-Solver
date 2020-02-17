aad58c95a6cbdcd26749b37ca144d36020296e6c
clarkmyfancy <clark.jack0@gmail.com>
2020-02-17 09:55:13 -0600

    refactoring code

--- Gui.py
+++ Gui.py
@@ -20,6 +20,10 @@ class Gui:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.gridHeight = (self.height - 4*Gui.fatLine) / Gui.numGridsPerBox
+        self.cellHeight = (self.gridHeight - 2*Gui.skinnyLine) / Gui.numCellsPerGrid
+        self.gridWidth = (self.width - 4*Gui.fatLine) / Gui.numGridsPerBox
+        self.cellWidth = (self.gridWidth - 2*Gui.skinnyLine) / Gui.numCellsPerGrid
 
     def renderPuzzle(self, window, grid, wasSolved):
         window.fill(Gui.WHITE)
@@ -35,11 +39,6 @@ class Gui:
         self.drawBorder(window)
 
     def drawThinGridLines(self, window):
-        gridHeight = (self.height - 4*Gui.fatLine) / Gui.numGridsPerBox
-        cellHeight = (gridHeight - 2*Gui.skinnyLine) / Gui.numCellsPerGrid
-        gridWidth = (self.width - 4*Gui.fatLine) / Gui.numGridsPerBox
-        cellWidth = (gridWidth - 2*Gui.skinnyLine) / Gui.numCellsPerGrid
-
         numCellsBehindCurrent = 0
         for i in range(6):
             if i % 2 == 0:
@@ -47,20 +46,18 @@ class Gui:
             else:
                 numCellsBehindCurrent = 2
             u = i // 2
-            firstGreyLineX = u*(gridWidth + Gui.fatLine) + Gui.fatLine + numCellsBehindCurrent*cellWidth + ((numCellsBehindCurrent - 1) * Gui.skinnyLine)
-            firstGreyLineY = u*(gridHeight + Gui.fatLine) + Gui.fatLine + numCellsBehindCurrent*cellHeight + ((numCellsBehindCurrent - 1) * Gui.skinnyLine)
+            firstGreyLineX = u*(self.gridWidth + Gui.fatLine) + Gui.fatLine + numCellsBehindCurrent*self.cellWidth + ((numCellsBehindCurrent - 1) * Gui.skinnyLine)
+            firstGreyLineY = u*(self.gridHeight + Gui.fatLine) + Gui.fatLine + numCellsBehindCurrent*self.cellHeight + ((numCellsBehindCurrent - 1) * Gui.skinnyLine)
             pygame.draw.rect(window, Gui.GRAY, (0, firstGreyLineY, self.width, Gui.skinnyLine))
             pygame.draw.rect(window, Gui.GRAY, (firstGreyLineX, 0, Gui.skinnyLine, self.height))
         
     def drawVerticalBoldedLines(self, window):
-        gridWidth = (self.width - 4*Gui.fatLine) / Gui.numGridsPerBox
-        pygame.draw.rect(window, Gui.BLACK, (gridWidth + Gui.fatLine, 0, Gui.fatLine, self.height))
-        pygame.draw.rect(window, Gui.BLACK, (2*gridWidth + 2*Gui.fatLine, 0, Gui.fatLine, self.height))
+        pygame.draw.rect(window, Gui.BLACK, (self.gridWidth + Gui.fatLine, 0, Gui.fatLine, self.height))
+        pygame.draw.rect(window, Gui.BLACK, (2*self.gridWidth + 2*Gui.fatLine, 0, Gui.fatLine, self.height))
     
     def drawHorizontalBoldedLines(self, window):
-        gridHeight = (self.height - 4*Gui.fatLine) / Gui.numGridsPerBox
-        pygame.draw.rect(window, Gui.BLACK, (0, gridHeight + Gui.fatLine, self.width, Gui.fatLine))
-        pygame.draw.rect(window, Gui.BLACK, (0, 2*gridHeight + 2*Gui.fatLine, self.width, Gui.fatLine))
+        pygame.draw.rect(window, Gui.BLACK, (0, self.gridHeight + Gui.fatLine, self.width, Gui.fatLine))
+        pygame.draw.rect(window, Gui.BLACK, (0, 2*self.gridHeight + 2*Gui.fatLine, self.width, Gui.fatLine))
     
     def drawBorder(self, window):
         pygame.draw.rect(window, Gui.BLACK, (0, 0, self.width, Gui.fatLine))
@@ -69,11 +66,6 @@ class Gui:
         pygame.draw.rect(window, Gui.BLACK, (self.width - Gui.fatLine, 0, self.width, self.height))
 
     def populateWithNumbers(self, window, grid):
-        gridHeight = (self.height - 4*Gui.fatLine) / Gui.numGridsPerBox
-        cellHeight = (gridHeight - 2*Gui.skinnyLine) / Gui.numCellsPerGrid
-        gridWidth = (self.width - 4*Gui.fatLine) / Gui.numGridsPerBox
-        cellWidth = (gridWidth - 2*Gui.skinnyLine) / Gui.numCellsPerGrid
-
         fnt = pygame.font.SysFont("comicsans", 40)
         for j, y in enumerate(grid):
             for i, x in enumerate(y):
@@ -82,11 +74,7 @@ class Gui:
                 text = fnt.render(str(x), 1, Gui.BLACK)
                 u = i // 3
                 v = j // 3
-                x = self.windowToCellTransformWidth(u, gridWidth) + ((i % 3)*(cellWidth + Gui.skinnyLine)) + 20
-                y = v*(gridHeight + Gui.fatLine) + Gui.fatLine + ((j % 3)*(cellHeight + Gui.skinnyLine)) + 20
+                x = u*(self.gridWidth + Gui.fatLine) + Gui.fatLine + ((i % 3)*(self.cellWidth + Gui.skinnyLine)) + 20
+                y = v*(self.gridHeight + Gui.fatLine) + Gui.fatLine + ((j % 3)*(self.cellHeight + Gui.skinnyLine)) + 20
                 window.blit(text, (x, y))
-
-    def windowToCellTransformWidth(self, u, gridWidth):
-        return u*(gridWidth + Gui.fatLine) + Gui.fatLine
- 
 
